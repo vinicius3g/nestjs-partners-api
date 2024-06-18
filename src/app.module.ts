115ffdd22cb145b69bbad87c8e7bb34c9dc1dd10
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-06-18 14:58:53 -0300

    feat: chamada dos modules de events e de conexão do prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [EventsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
