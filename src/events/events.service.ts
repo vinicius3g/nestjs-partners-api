83eb462a7d6ccbea15383fd7dcd66ef330466893
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-06-18 14:57:15 -0300

    feat: confugaraçao das chamadas e modulos, service, controller, module  do serviço de events para populaçao e manipulação do mesmo

---
+++ src/events/events.service.ts
@@ -0,0 +1,39 @@
+import { HttpCode, Injectable } from '@nestjs/common';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class EventsService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(createEventDto: CreateEventDto) {
+    return this.prismaService.event.create({
+      data: createEventDto,
+    });
+  }
+
+  findAll() {
+    return this.prismaService.event.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prismaService.event.findUnique({
+      where: { id },
+    });
+  }
+
+  update(id: string, updateEventDto: UpdateEventDto) {
+    return this.prismaService.event.update({
+      data: updateEventDto,
+      where: { id },
+    });
+  }
+
+  @HttpCode(204)
+  remove(id: string) {
+    return this.prismaService.event.delete({
+      where: { id },
+    });
+  }
+}
