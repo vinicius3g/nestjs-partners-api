61a70499244ce5381189802db63cb3d1de6a584e
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-06-19 20:25:17 +0000

    feat: export do serviços da lib de modo global

--- libs/core/src/events/events-core.module.ts
+++ libs/core/src/events/events-core.module.ts
@@ -3,5 +3,6 @@ import { EventsService } from './events-core.service';
 
 @Module({
   providers: [EventsService],
+  exports: [EventsService],
 })
 export class EventsCoreModule {}
--- libs/core/src/spots/spots-core.module.ts
+++ libs/core/src/spots/spots-core.module.ts
@@ -3,5 +3,6 @@ import { SpotsService } from './spots-core.service';
 
 @Module({
   providers: [SpotsService],
+  exports: [SpotsService],
 })
 export class SpotsCoreModule {}
